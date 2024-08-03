75dc4e8fbf4a50987854b021a1da4b4faab42529
Ifham-Hashir <ifham09052003@gmail.com>
2024-08-03 20:28:31 +0530

    Add fonts and images

---
+++ fonts/stylesheet.css
@@ -0,0 +1,9 @@
+@font-face {
+    font-family: 'Norse';
+    src: url('Norse-Bold.woff2') format('woff2'),
+        url('Norse-Bold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+}
+
